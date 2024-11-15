dfd975f2a7ae95807033e49562f754d1d11d5fe7
Phan Lam Hung <lamhungphan1999@gmail.com>
2024-11-15 12:09:53 +0700

    spriderum ui

---
+++ assignment/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Navbar from './components/Nav.vue'
+</script>
+
+<template>
+  <Navbar/>
+  <RouterView/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ assignment/src/components/Main.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="site-inner">
+    <main class="content">
+        <article>
+
+        </article>
+    </main>
+    <aside class="sidebar">
+        <section>
+            
+        </section>
+    </aside>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
